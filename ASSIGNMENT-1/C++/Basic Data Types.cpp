d3989b1953c3ed3a5c05f3bafbfb987bcbe1d4f5
AdityaS-21 <114082319+AdityaS-21@users.noreply.github.com>
2022-09-21 19:08:44 +0530

    Create Basic Data Types.cpp

---
+++ ASSIGNMENT-1/C++/Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int num;
+    cin>>num;
+    long l;
+    cin>>l;
+    char character;
+    cin>>character;
+    float decimal;
+    cin>>decimal;
+    double d;
+    cin>>d;
+    cout<<num<<endl<<l<<endl<<character<<endl<<decimal<<endl<<d<<endl;
+    
+    return 0;
+}
